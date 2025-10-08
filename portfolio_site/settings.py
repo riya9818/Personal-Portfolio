2cc1b1caac971a032811556fe6fcbbdd694fb7b1
riya9818 <reeyabasnet999@gmail.com>
2025-10-08 12:48:50 +0545

    working core htmls

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -136,7 +136,7 @@ env = environ.Env(
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-DEBUG = env('DJANGO_DEBUG')
+DEBUG = True
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
