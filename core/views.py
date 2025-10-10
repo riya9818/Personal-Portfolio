34dbbdce3fb8f3a3faec6cd666abfb8687139d32
riya9818 <reeyabasnet999@gmail.com>
2025-10-10 15:07:03 +0545

    updated views to retrieve from admin

--- core/views.py
+++ core/views.py
@@ -2,12 +2,32 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.contrib import messages
 from .forms import ContactForm
+from django.shortcuts import render, get_object_or_404
+from .models import Testimonial, BlogPost
+from projects.models import Project
 
 def home(request):
-    return render(request, 'core/home.html')
+    projects = Project.objects.all()[:3]  # latest 3 projects
+    testimonials = Testimonial.objects.all()[:3]
+    return render(request, 'core/home.html', {'projects': projects, 'testimonials': testimonials})
+
+
 
 def about(request):
     return render(request, 'core/about.html')
+
+
+def testimonials_view(request):
+    testimonials = Testimonial.objects.all()
+    return render(request, 'core/testimonials.html', {'testimonials': testimonials})
+
+def blog_list(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'core/blog_list.html', {'posts': posts})
+
+def blog_detail(request, slug):
+    post = get_object_or_404(BlogPost, slug=slug)
+    return render(request, 'core/blog_detail.html', {'post': post})
 
 def contact(request):
     if request.method == 'POST':
