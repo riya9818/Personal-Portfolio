c5b22d8a2b52c2ef8cc132a78fd94e53f629f355
riya9818 <reeyabasnet999@gmail.com>
2025-10-13 20:39:38 +0545

    correct folder tree

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -108,7 +108,12 @@ USE_TZ = True
 # ---------------------------------------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static",
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
